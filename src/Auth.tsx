d9930ac1aaf65c98fb10bdd6f85152c8995ed5b3
Swag-blips <coderblip@gmail.com>
2024-11-13 18:42:40 +0100

    feat:setup-blog

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,49 +1,43 @@
 import { useState } from "react";
-import { supabase } from "./supabase";
+import "./Auth.css";
 
 export default function Auth() {
-  const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    firstName: "",
+    lastName: "",
+    password: "",
+  });
 
-  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    setLoading(true);
-    const { error } = await supabase.auth.signInWithOtp({ email });
-
-    if (error) {
-      alert(error.code || error.message);
-    } else {
-      alert("Check your email for the login link!");
-    }
-    setLoading(false);
-  };
-
+  const handleChange = (e: React.ChangeEvent)
   return (
-    <div className="row flex flex-center">
-      <div className="col-6 form-widget">
-        <h1 className="header">Supabase + React</h1>
-        <p className="description">
-          Sign in via magic link with your email below
-        </p>
-        <form className="form-widget" onSubmit={handleLogin}>
-          <div>
-            <input
-              className="inputField"
-              type="email"
-              placeholder="Your email"
-              value={email}
-              required={true}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <button className={"button block"} disabled={loading}>
-              {loading ? <span>Loading</span> : <span>Send magic link</span>}
-            </button>
-          </div>
-        </form>
-      </div>
+    <div>
+      <form>
+        <input
+          className="form-input"
+          type="email"
+          name="email"
+          placeholder="email"
+        />
+        <input
+          className="form-input"
+          type="passowrd"
+          name="password"
+          placeholder="password"
+        />
+        <input
+          className="form-input"
+          type="text"
+          name="firstName"
+          placeholder="first name"
+        />
+        <input
+          className="form-input"
+          type="text"
+          name="lastName"
+          placeholder="last name"
+        />
+      </form>
     </div>
   );
 }
