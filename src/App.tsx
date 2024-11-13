e8b9bd29bd3a173a5a616c0c2dcdbd8c9d2374c2
Swag-blips <coderblip@gmail.com>
2024-11-13 15:07:13 +0100

    feat:changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
+import "./App.css";
 import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Session } from "@supabase/supabase-js";
 
 const supaBase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_API_KEY
 );
 function App() {
-  const [session, setSession] = useState(null);
-  return (
-    <>
-      <Auth supabaseClient={supaBase} appearance={{ theme: ThemeSupa }} />
-    </>
-  );
+  const [session, setSession] = useState<Session | null>();
+
+  useEffect(() => {
+    supaBase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supaBase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  console.log(session);
+
+  if (session) {
+    return <p>You are logged in</p>;
+  }
+  return <Auth supabaseClient={supaBase} appearance={{ theme: ThemeSupa }} />;
 }
 
 export default App;
