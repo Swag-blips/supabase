5e267e1fe28bc41e35867fa11044e8ebe59f38d7
Swag-blips <coderblip@gmail.com>
2024-11-15 21:37:06 +0100

    feat:

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import "./Auth.css";
+import supabase from "./supabase";
 
 export default function Auth() {
   const [formData, setFormData] = useState({
     email: "",
-    firstName: "",
-    lastName: "",
     password: "",
   });
 
@@ -13,10 +12,31 @@ export default function Auth() {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
-  console.log(formData);
+
+  const handleSignUp = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(supabase);
+
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: formData.email,
+        password: formData.password,
+        // options: {
+        //   emailRedirectTo: "http://localhost:5173",
+        // },
+      });
+
+      if (data) {
+        console.log(data);
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <div>
-      <form className="form">
+      <form onSubmit={handleSignUp} className="form">
         <input
           className="form-input"
           type="email"
@@ -31,20 +51,8 @@ export default function Auth() {
           onChange={handleChange}
           placeholder="password"
         />
-        <input
-          className="form-input"
-          type="text"
-          name="firstName"
-          onChange={handleChange}
-          placeholder="first name"
-        />
-        <input
-          className="form-input"
-          type="text"
-          name="lastName"
-          onChange={handleChange}
-          placeholder="last name"
-        />
+
+        <button onClick={handleSignUp}>sign Up</button>
       </form>
     </div>
   );
